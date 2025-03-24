c4ab47e8e5dc061a4f1dc00e44c95f477cf24e66
Rupam797 <rupamgiri318@gmail.com>
2025-03-24 06:37:29 +0530

    Daily update Mon Mar 24 06:37:29 IST 2025

--- TravoNest/main.js
+++ TravoNest/main.js
@@ -61,3 +61,10 @@ ScrollReveal().reveal(".room__card", {
   ...scrollRevealOption,
   interval: 500,
 });
+
+// service container
+ScrollReveal().reveal(".service__list li", {
+  ...scrollRevealOption,
+  interval: 500,
+  origin: "right",
+});
